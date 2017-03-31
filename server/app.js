1ca97da0248b4424dc878f4e41ec7fe70cf18406
Chris Keller <cjameskeller@gmail.com>
2017-03-31 13:54:07 -0500

    fixed

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 
 //sets port automatically to whatever heroku sets it as
 app.set("port", (process.env.PORT || 5000));
+
+app.use(express.static("./server/public/"));
 
 app.get("/", function(req, res)
 {
